63171d02f74efcef75ba91058ef0be5b525ddd85
Kittiphon Kamnuan <kittiphon.kam@dome.tu.ac.th>
2023-11-11 11:25:06 +0700

    Change to return a.reduce

--- math.js
+++ math.js
@@ -1,7 +1,6 @@
 function sum(a, b) {
 	if (Array.isArray(a)) {
-		let sum = 0
-		a.forEach(i) => (sum += i))
+		return a.reduce(acc, n) => acc + n, 0)
 		return sum
 	}	else if (typeof a === 'number' && typeof b === 'number') {
 		return a + b
