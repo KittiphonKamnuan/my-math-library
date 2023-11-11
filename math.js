2b808e0b43d8f4fbd45a217c18b779320f3cc3b1
Kittiphon Kamnuan <kittiphon.kam@dome.tu.ac.th>
2023-11-11 11:38:57 +0700

    fix error

--- math.js
+++ math.js
@@ -1,7 +1,6 @@
 function sum(a, b) {
 	if (Array.isArray(a)) {
 		return a.reduce(acc, n) => acc + n, 0)
-		return sum
 	}	else if (typeof a === 'number' && typeof b === 'number') {
 		return a + b
 	}
